ac4cfb305d67ba0fbb68bdb62fed86c081efe36f
shrutishinde249 <shrutishinde249@gmail.com>
2022-02-08 12:10:57 +0000

    done

--- angf-e/src/app/MyComponents/billing_ins/drag-drop-comp/drag-drop-comp.component.ts
+++ angf-e/src/app/MyComponents/billing_ins/drag-drop-comp/drag-drop-comp.component.ts
@@ -13,32 +13,23 @@ export class DragDropCompComponent{
   @ViewChild("listbox1", { static: false }) public listObj1:any;
   @ViewChild("listbox2", { static: false }) public listObj2:any;
   public dataA: { [key: string]: Object }[] = [
-    { Name: "Australia", Code: "AU" },
-    { Name: "Bermuda", Code: "BM" },
-    { Name: "Canada", Code: "CA" },
-    { Name: "Cameroon", Code: "CM" },
-    { Name: "Denmark", Code: "DK" },
-    { Name: "France", Code: "FR" },
-    { Name: "Finland", Code: "FI" },
-    { Name: "Germany", Code: "DE" },
-    { Name: "Hong Kong", Code: "HK" }
+    { Name: "InlandMarine", Code: "In" },
+    { Name: "Casualty", Code: "Ca" },
+    { Name: "Other", Code: "Ot" },
+    { Name: "Package", Code: "Pac" },
+    { Name: "Executive Risk", Code: "Exe" },
+    { Name: "Aviation", Code: "Avi" },
+    { Name: "Surety", Code: "Sur" },
+    { Name: "Marine", Code: "Mar" },
+    { Name: "Enviornmental", Code: "Env" }
   ];
-  public dataB: { [key: string]: Object }[] = [
-    { Name: "India", Code: "IN" },
-    { Name: "Italy", Code: "IT" },
-    { Name: "Japan", Code: "JP" },
-    { Name: "Mexico", Code: "MX" },
-    { Name: "Norway", Code: "NO" },
-    { Name: "Poland", Code: "PL" },
-    { Name: "Switzerland", Code: "CH" },
-    { Name: "United Kingdom", Code: "GB" },
-    { Name: "United States", Code: "US" }
-  ];
+  public dataB: { [key: string]: Object }[] = [];
+  
   public fields: FieldSettingsModel = { text: "Name" };
   public toolbarSettings: ToolbarSettingsModel = {
     items: [
-      "moveUp",
-      "moveDown",
+      //"moveUp",
+      //"moveDown",
       "moveTo",
       "moveFrom",
       "moveAllTo",
