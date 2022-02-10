7741ebf40250364be254c9c42a4d33c7b545ddb7
shrutishinde249 <shrutishinde249@gmail.com>
2022-02-10 06:30:49 +0000

    done

--- angf-e/src/app/MyComponents/billing_ins/modificationcomp/modificationcomp.component.ts
+++ angf-e/src/app/MyComponents/billing_ins/modificationcomp/modificationcomp.component.ts
@@ -23,7 +23,7 @@ export class ModificationcompComponent implements OnInit {
   email!:string
   id!:number
   instr_level!:string
-  
+ li!:string
   public ird_account:boolean=true
   public ird_contact:boolean=false
   constructor(public service: ClientApiService,private router: Router) { }
@@ -31,6 +31,7 @@ export class ModificationcompComponent implements OnInit {
   ngOnInit(): void {
     this.dataA;
     this.dataB;
+    
   }
   
    ird(){
@@ -44,15 +45,15 @@ export class ModificationcompComponent implements OnInit {
 @ViewChild("listbox1", { static: false }) public listObj1:any;
 @ViewChild("listbox2", { static: false }) public listObj2:any;
 public dataA: { [key: string]: Object }[] = [
-  { Name: "InlandMarine", Code: "In" },
-  { Name: "Casualty", Code: "Ca" },
-  { Name: "Other", Code: "Ot" },
-  { Name: "Package", Code: "Pac" },
-  { Name: "Executive Risk", Code: "Exe" },
-  { Name: "Aviation", Code: "Avi" },
-  { Name: "Surety", Code: "Sur" },
-  { Name: "Marine", Code: "Mar" },
-  { Name: "Enviornmental", Code: "Env" }
+  { Name: "InlandMarine"},
+  { Name: "Casualty"},
+  { Name: "Other"},
+  { Name: "Package"},
+  { Name: "Executive Risk"},
+  { Name: "Aviation" },
+  { Name: "Surety"},
+  { Name: "Marine" },
+  { Name: "Enviornmental"}
 ];
 public dataB: { [key: string]: Object }[]=[];
 
@@ -67,7 +68,8 @@ public toolbarSettings: ToolbarSettingsModel = {
 };
 click() {
   var data = this.listObj2.getDataList();
- 
+ //this.li.push(data[0].Name);
+ this.li=(data[0].Name)
   this.dataB.push(data);
   delete this.dataA[data];
   console.log(this.dataA);
@@ -80,26 +82,28 @@ actionComplete(args: any) {
   console.log(args);
 }
 
+
   submit(){
     var data={
       instr_level:this.instr_level,
       agency_code:'test code',
       s_id:'01',
       contact_name:this.item.client_name,
-      glob:this.dataB[0],
+      glob:this.li,
       desc:this.desc,
       updated_by:'test user',
       updated_on:this.date,
       client_inv_del:null,
       email:this.item.email,
-      
     }
-    
+   
     this.id=this.item.id;
-    console.log(data);
+    
+    //data.glob[0]=data.glob;
     this.service.clientmeta(this.id,data).subscribe(res=>{
       
-      
+      console.log(data)
+      this.router.navigateByUrl('Home');
     })
   }
 }
