a58dacf96ed095747eacbd2668c70bf278e7a15f
shrutishinde249 <shrutishinde249@gmail.com>
2022-02-15 05:32:20 +0000

    sorting done

--- angf-e/src/app/MyComponents/billing_ins/final-data/final-data.component.ts
+++ angf-e/src/app/MyComponents/billing_ins/final-data/final-data.component.ts
@@ -1,22 +1,9 @@
-import { Component,Input, OnInit,ViewChild } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { NetApiService } from 'src/app/Services/net-api.service';
 import { ClientApiService } from 'src/app/Services/client-api.service';
-import {  MatSort ,Sort} from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { BehaviorSubject } from 'rxjs';
-
-export interface finalData{
-  instr_level:string;
-  agency_code:string;
-  s_id:number;
-  contact_name:string;
-  glob:string;
-  desc:string;
-  updated_by:string;
-  updated_on:Date;
-}
-
-//const Element_Data :finalData[]=[];
+import {Sort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-final-data',
@@ -26,42 +13,48 @@ export interface finalData{
 export class FinalDataComponent implements OnInit {
   @Input() item!:any
   x:any
-  
-  displayedColumns: string[]=['level','code','siteId','contact','glob','desc','updatedby','updatedon'];
-  dataTableElements:finalData[]=[];
-  //dataSource=new MatTableDataSource(Element_Data);
-  dataSubject = new BehaviorSubject<Element[]>([]);
 
-  //@ViewChild(MatSort) sort!: MatSort;
-  constructor(public service: ClientApiService,private route: ActivatedRoute,private router: Router){ }
+  constructor(public service: ClientApiService,private route: ActivatedRoute,private router: Router){
+  }
 
   ngOnInit(): void {
-    //this.dataSource.sort=this.sort;
   }
 
   show(data:any){
     this.x=data
   }
-  
-  onSortData(sort: Sort) {
-    let data = this.dataTableElements.slice();
-    if (sort.active && sort.direction !== '') {
-        data = data.sort((a: finalData, b: finalData) => {
-            const isAsc = sort.direction === 'asc';
-            switch (sort.active) {
-                case 'level': return this.compare(a.instr_level, b.instr_level, isAsc);
-                case 'code': return this.compare(+a.agency_code, +b.agency_code, isAsc);
-                case 'siteId': return this.compare(+a.s_id, +b.s_id, isAsc);
-                case 'contact': return this.compare(+a.contact_name, +b.contact_name, isAsc);
-                default: return 0;
-            }
-        });
+
+  sortData(sort: Sort) {
+    if (!sort.active || sort.direction === '') {
+      return this.item.client_inv_del.$values;
     }
-    this.dataSubject.next;
-}
 
-private compare(a: string | number, b: string | number, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-}
+    this.item.client_inv_del.$values = this.item.client_inv_del.$values.sort((a:any, b:any) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'level':
+          return this.compare(a.inv_delivery.instr_level, b.inv_delivery.instr_level, isAsc);
+          case 'code':
+          return this.compare(a.inv_delivery.agency_code, b.inv_delivery.agency_code, isAsc);
+          case 'siteId':
+          return this.compare(a.inv_delivery.s_id, b.inv_delivery.s_id, isAsc);
+          case 'contact':
+          return this.compare(a.inv_delivery.contact_name, b.inv_delivery.contact_name, isAsc);
+          case 'glob':
+          return this.compare(a.inv_delivery.glob, b.inv_delivery.glob, isAsc);
+          case 'desc':
+          return this.compare(a.inv_delivery.desc, b.inv_delivery.desc, isAsc);
+          case 'updatedby':
+          return this.compare(a.inv_delivery.updated_by, b.inv_delivery.updated_by, isAsc);
+          case 'updated on':
+          return this.compare(a.inv_delivery.updated_on, b.inv_delivery.updated_on, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
 
+  compare(a: number | string | any, b: number | string | any, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
 }
